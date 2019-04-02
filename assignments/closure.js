19f8ff88102ccafc63014d2be70f5edfb8503a47
TinaDelli <cecurrier92@gmail.com>
2019-04-02 17:19:33 -0400

    edited array method and closer

--- assignments/closure.js
+++ assignments/closure.js
@@ -1,6 +1,27 @@
 // ==== Challenge 1: Write your own closure ====
 // Write a simple closure of your own creation.  Keep it simple!
 
+function destinyTwo() {
+  const hero = "Hero";
+  console.log(`Help us find all our ghosts ${hero}`);
+  
+  function planets() {
+    const ghost = "Last Ghost to Survive";
+    console.log(`We have no ${hero} to protect us! Please protect us with ${ghost}`);
+
+    function mission() {
+      console.log(`The bad guy wonders how you obtained ${ghost}`);
+      console.log(`The planets will forever know you as ${hero}`);
+      debugger;
+    }// mission
+    
+    mission();
+  }// planets
+  
+  planets();
+}// destinyTwo
+
+destinyTwo();
 
 /* STRETCH PROBLEMS, Do not attempt until you have completed all previous tasks for today's project files */
 
